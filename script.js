1a58219a4e091c536ae91b884c4b3a27da7ccb93
Colin Lewandowski <75598914+CLewi1@users.noreply.github.com>
2023-02-19 19:36:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
+// Define an array of quotes
 const quotes = [
-	"Believe you can and you're halfway there. -Theodore Roosevelt",
-	"The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
-	"Success is not final, failure is not fatal: it is the courage to continue that counts. -Winston Churchill",
-	"Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy. -Norman Vincent Peale",
-	"Perfection is not attainable, but if we chase perfection we can catch excellence. -Vince Lombardi",
-	"Believe that life is worth living and your belief will help create the fact. -William James",
-	"You miss 100% of the shots you don't take. -Wayne Gretzky",
-	"Don't watch the clock; do what it does. Keep going. -Sam Levenson",
-	"Success is walking from failure to failure with no loss of enthusiasm. -Winston Churchill",
-	"The only way to do great work is to love what you do. -Steve Jobs"
-];
-
-const quoteElem = document.getElementById("quote");
-const generateBtn = document.getElementById("generate-btn");
-
-function generateQuote() {
-	const randomIndex = Math.floor(Math.random() * quotes.length);
-	const randomQuote = quotes[randomIndex];
-	quoteElem.innerHTML = randomQuote;
-}
-
-// Call generateQuote function when page loads
-generateQuote();
-
-generateBtn.addEventListener("click", generateQuote);
-
-setInterval(generateQuote, 3600000);
+	"Even your shadow leaves you at night... ",
+	"Don't tell me the sky is the limit when there are footprints on the moon.",
+	"A dream is worth nothing if you leave it on the pillow.",
+	"Don't fear the next chapter, you already know the author.",
+	"Average is the enemy, success is your responsibility, and change can happen in an instant, if you are willing to flip the switch.",
+	"Give a man a purpose and the ability to achieve it, and he will crawl over broken glass with a smile.",
+	"You don't become confident by shouting affirmations in the mirror. But by having a stack of undeniable proof that you are who you say you are. Outwork your self doubt.",
+	"All exceptional and mentally strong people have been through hell. Very few put themselves through hell when life is great.",
+	"You are always afraid to take the first step because all you see is every negative thing 10 miles down the road. But you can do anything you want. You are bound by nothing.",
+	"If it was easy, everyone would do it.",
+	"Believe in yourself and all that you are. Know there is something inside you that is greater than any obstacle.",
+	"There are two people. The man who thinks they can, and the man who thinks they can't. They're both right.",
+	"Your life is a reflection of your thoughts. If you want to change your life, change your thoughts.",
+	"You can't win the war against the world if you can't win the war against your own mind.",
+	"Bravery is not the absence of fear. Bravery is feeling fear and moving forward anyway.",
+	"Dreams without goals are just dreams. And dreams are cheap.",
+	"Why take life so seriously? You're not going to get out alive anyway.",
+	"Life is not about finding yourself. Life is about creating yourself.",
+	"One day it will all make sense. So for now, laugh at the confusion, smile through the tears, and keep reminding yourself that everything happens for a reason.",
+	"You get what you work for, not what you wish for.",
+	"Excuses make today easy, but tomorrow hard. Discipline makes today hard, but tomorrow easy.",
+	"You are the only person who can make this work. You are also the only person who can fuck this up. It's you vs you.",
+	"If you try, you risk failure. If you don't try, you guarantee it.",
+	"Watch your thoughts, they become your words. Watch your words, they become your actions. Watch your actions, they produce your future.",
+	"Let the past die. Kill it if you have to. That's the only way to become what you were meant to be.",
+	"You may be in your darkest hour. Know that night don't last forever. There's a morning coming in your life.",
+	"Change your mindset today. Stop bitching. No ones listening. Nobody gives a fuck. Stop expecting shit. And take what's yours, by having the humility to put in the work.", 
+	"Every man has 2 lives. The second one starts when he realizes he only has one.",
+  ];
+  
+  // Get a reference to the quote element
+  const quoteElement = document.getElementById("quote");
+  
+  // Define a function to generate a random quote
+  function generateQuote() {
+	// Get a random quote from the quotes array
+	const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+	// Update the quote element with the new quote
+	quoteElement.innerText = randomQuote;
+  }
+  
+  // Call the generateQuote function once to display an initial quote
+  generateQuote();
+  
+  // Add a click event listener to the quote element to change the quote on click
+  quoteElement.addEventListener("click", generateQuote);
+  
